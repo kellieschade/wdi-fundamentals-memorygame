0b9952e51e639a71659c94446e17d883144432cb
Schade <KS83452@AD1.PROD>
2018-05-03 10:57:27 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,25 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne );
-console.log("User flipped " + cardThree );
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push("cardOne");
+console.log("user flipped queen");
+var cardTwo = cards[2];
+cardsInPlay.push("cardTwo");
+console.log("user flipped king");
+if (cardsInPlay.length === 2) 
+if (cardsInPlay[0] === cardsInPlay[1]){
+alert("You found your match!");
+} else {
+    alert("Sorry try again.");
+}
+
+
+
+
+
+
+
+
+
+

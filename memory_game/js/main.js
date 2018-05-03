ea3b4cb122d749b22594dec4e2922d8000fc7299
Schade <KS83452@AD1.PROD>
2018-05-03 14:17:08 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -11,10 +11,11 @@ var flipCard = function (cardId){
     console.log("User flipped " + cards[cardId]);
     cardsInPlay.push(cards[cardId]);
 
+    checkForMatch ();
     
 }
 flipCard (0);
-
+flipCard (2);
 
 
 

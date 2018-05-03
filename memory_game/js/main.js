ec20b6ba877a3dde924808a5f789bb294764a555
Schade <KS83452@AD1.PROD>
2018-05-03 17:47:19 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,29 @@
 console.log("Up and running!");
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png",
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamons.png",
+    },    
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "image/king-of-hearts.png",
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png",
+    } 
+];  
+
+    
+
 var cardsInPlay = [];
 var checkForMatch = function () { 
     if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -8,14 +32,20 @@ var checkForMatch = function () {
             alert("Sorry try again.");
 }}
 var flipCard = function (cardId){
-    console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
+    console.log("User flipped " + cards[cardId].cardImage);
+    console.log("User flipped " + cards[cardId].suit);
+    
+    cardsInPlay.push(cards[cardId].rank);
 
     checkForMatch ();
     
 }
 flipCard (0);
+flipCard (1);
 flipCard (2);
+flipCard (3);
+
 
 
 

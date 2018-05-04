93044153c946adfa73ae2357ac0032fcf237e040
Schade <KS83452@AD1.PROD>
2018-05-04 11:53:30 -0400

    finalproject

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,50 +1,106 @@
-console.log("Up and running!");
-var cards = [
-    {
-        rank: "queen",
-        suit: "hearts",
-        cardImage: "images/queen-of-hearts.png",
-    },
-    {
-        rank: "queen",
-        suit: "diamonds",
-        cardImage: "images/queen-of-diamons.png",
-    },    
-    {
-        rank: "king",
-        suit: "hearts",
-        cardImage: "image/king-of-hearts.png",
-    },
-    {
-        rank: "king",
-        suit: "diamonds",
-        cardImage: "images/king-of-diamonds.png",
-    } 
-];  
+
+
+    var cards = [
+        {
+            rank: "queen",
+            suit: "hearts",
+            cardImage: "images/queen-of-hearts.png",
+        },
+        {
+            rank: "queen",
+            suit: "diamonds",
+            cardImage: "images/queen-of-diamons.png",
+        },
+        {
+            rank: "king",
+            suit: "hearts",
+            cardImage: "image/king-of-hearts.png",
+        },
+        {
+            rank: "king",
+            suit: "diamonds",
+            cardImage: "images/king-of-diamonds.png",
+        }
+    ];
+
+
+
+        var cardsInPlay = [];
+        
 
     
 
-var cardsInPlay = [];
-var checkForMatch = function () { 
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-        alert("You found your match!");
-     } else {
-            alert("Sorry try again.");
-}}
-var flipCard = function (cardId){
-    console.log("User flipped " + cards[cardId].rank);
-    console.log("User flipped " + cards[cardId].cardImage);
-    console.log("User flipped " + cards[cardId].suit);
-    
-    cardsInPlay.push(cards[cardId].rank);
+    var flipCard = function () {
+        var cardId = this.getAttribute('data-id');
+        
+        cardsInPlay.push(cards.rank);
+        this.document.getElementById("src");
 
-    checkForMatch ();
+
+        console.log("User flipped " + cards[cardId].rank);
+        console.log("User flipped " + cards[cardId].cardImage);
+        console.log("User flipped " + cards[cardId].suit);
+        checkForMatch()
+    }
+
+
+
+
+
+        var checkForMatch = function () {
+        
+        this.setAttribute(cards[cardId].cardImage)
+
+        if (cardsInPlay.length === 2)
+            if (cardsInPlay[0] === cardsInPlay[1]) {
+                alert("You found your match!");
+            } else {
+                alert("Sorry try again.");
+            }
+        }
+
+
     
-}
-flipCard (0);
-flipCard (1);
-flipCard (2);
-flipCard (3);
+
+        var createBoard = function () {
+            for (var i = 0; i < cards.length; i++) {
+                console.log(i);
+
+                
+            var cardElement = document.createElement('img');
+            
+            cardElement.setAttribute('src',"images/back.png");
+            cardElement.setAttribute("game-board", i);
+            cardElement.setAttribute('data-id', i);
+            cardElement.addEventListener('click', flipCard);
+            document.getElementById('game-board').appendChild(cardElement);
+
+            }
+
+        }
+    
+
+
+
+createBoard();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
